6bb2d3fb5e2fe55e2898a619bfa3ebc285d7fbd0
João Paulo <j.sasdelli@escola.pr.gov.br>
2026-02-20 07:47:12 -0300

    Initialize pingpong.js with basic setup

---
+++ pingpong.js
@@ -0,0 +1,24 @@
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
+
+let velocidadeXbolinha = 6;
+let velocidadeYBolinha = 6;
+
+let xRaquete = 5;
+let yRaquete = 150;
+
+let XRaqueteOponente = 585;
+let YRaqueteOponente = 150;
+
+let meusPontos = 0;
+let pontosOponente = 0;
+
+function setup(){
+  createCanvas(600, 400)
+}
+
+function draw(){
+  
+}
